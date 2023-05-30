0c52f957608a8adc98f4e8b20ccc060fcaef7a59
Serhii Halkin <galkindev@gmail.com>
2023-05-30 09:36:03 +0100

    Copy env files before composer install to fix an error DATABASE_URL variable not found when clear:cache command executing

--- docker/php/config/docker-entrypoint.sh
+++ docker/php/config/docker-entrypoint.sh
@@ -7,18 +7,18 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
 
+cp /var/www/.env.local /var/www/app/.env.local
+cp /var/www/.env.test.local /var/www/app/.env.test.local
+
 if [ "$1" = 'php-fpm' ]; then
     # Composer install on service run
     composer install --prefer-dist --no-interaction --optimize-autoloader
 fi
 
-cp /var/www/.env.local /var/www/app/.env.local
-cp /var/www/.env.test.local /var/www/app/.env.test.local
-
 php bin/console cache:clear
 
-php bin/console doctrine:database:drop --force
-php bin/console doctrine:database:drop --env=test --force
+php bin/console doctrine:database:drop --if-exists --force
+php bin/console doctrine:database:drop --if-exists --env=test --force
 
 php bin/console doctrine:database:create --if-not-exists --no-interaction
 php bin/console doctrine:database:create --env=test --if-not-exists --no-interaction
